1effaf75495ee999fd027cb67bfb33590e918b53
Caique Moura <caique.fred@outlook.com>
2023-11-26 13:27:20 -0300

    update atividades

--- Algoritmos e Logica de Programação/Atividades Praticas/src/atividade4.js
+++ Algoritmos e Logica de Programação/Atividades Praticas/src/atividade4.js
@@ -1,100 +1,120 @@
-{
-  // Exercicio1
-  function quebrar(nome) {
-    return nome.split("");
-  }
+// {
+//   // Exercicio1 : quebrar a string com o atributo split
+//   function quebrar(nome) {
+//     return nome.split("");
+//   }
 
-  let r = quebrar("Pedro");
-  console.log("[1]Separado:", r);
-}
+//   let r = quebrar("Pedro");
+//   console.log("[1]Separado:", r);
+// }
 
-{
-  //Exercicio2
-  function quebrar(nome) {
-    const v = nome.split("");
-    for (let i = 0; i < v.length; i++) {
-      if (v[i] == "a") {
-        v[i] = "-";
-      }
+// {
+//   //Exercicio2
+//   function quebrar(nome) {
+//     const v = nome.split("");
+//     for (let i = 0; i < v.length; i++) {
+//       if (v[i] == "a") {
+//         v[i] = "-";
+//       }
+//     }
+//     return v;
+//   }
+//   let r = quebrar("Mariana");
+//   console.log("[2]Separado:", r);
+// }
+
+// {
+//   //Exercicio3
+//   function quebrar(nome) {
+//     const v = nome.split("");
+//     for (let i = 0; i < v.length; i++) {
+//       if (
+//         v[i] == "a" ||
+//         v[i] == "e" ||
+//         v[i] == "i" ||
+//         v[i] == "o" ||
+//         v[i] == "u"
+//       ) {
+//         v[i] = "-";
+//       }
+//     }
+//     return v;
+//   }
+//   let r = quebrar("Mariana");
+//   console.log("Separado:", r);
+// }
+
+// {
+//   //exercicio4
+//   function quebrar(nome) {
+//     let v = nome.split("");
+//     for (let i = 0; i < v.length; i++) {
+//       v[i] = "<div>" + v[i] + "<div>";
+//     }
+//     return v;
+//   }
+
+//   let r = quebrar("Mariana");
+//   console.log("Separado:", r);
+// }
+
+// {
+//   //exercicio5
+//   function criar(a, b) {
+//     let v = []; //cria um array vazio
+//     for (let i = 0; a <= b; i++, a++) {
+//       (v[i] = a), b;
+//     }
+//     return v;
+//   }
+//   let r = criar(5, 10);
+//   console.log("Array:", r);
+// }
+
+// {
+//   //exercicio6
+//   function criar(a, b) {
+//     let v = []; //cria um array vazio
+//     for (let i = 0; a <= b; i++, a++) {
+//       v[i] = a + "";
+//     }
+//     return v;
+//   }
+//   let r = criar(5, 10);
+//   console.log("Array:", r);
+// }
+
+// {
+//   //exercicio7
+//   function somar(a, b) {
+//     let v = []; //cria um array vazio
+//     for (let i = 0; i < a; i++) {
+//       v[i] = a[i] + b[i];
+//     }
+//     return;
+//   }
+//   let a = [5, 8, 3, 4, 9, 7];
+//   let b = [3, 5, 9, 2, 3, 4];
+//   let r = somar(a, b);
+//   console.log("Soma:", r);
+// }
+
+
+//exercicio 8
+function somar (a,b) {
+  let v = [];
+  for (let i = 0; i < a.length; i++) {
+    if ( a[i] < b[i]) {
+      v[i] = b[i];
     }
-    return v;
-  }
-  let r = quebrar("Mariana");
-  console.log("[2]Separado:", r);
-}
-
-{
-  //Exercicio3
-  function quebrar(nome) {
-    const v = nome.split("");
-    for (let i = 0; i < v.length; i++) {
-      if (
-        v[i] == "a" ||
-        v[i] == "e" ||
-        v[i] == "i" ||
-        v[i] == "o" ||
-        v[i] == "u"
-      ) {
-        v[i] = "-";
-      }
+    else {
+      v[i] = a[i];
     }
-    return v;
   }
-  let r = quebrar("Mariana");
-  console.log("Separado:", r);
+  return v;
 }
 
-{
-  //exercicio4
-  function quebrar(nome) {
-    let v = nome.split("");
-    for (let i = 0; i < v.length; i++) {
-      v[i] = "<div>" + v[i] + "<div>";
-    }
-    return v;
-  }
-
-  let r = quebrar("Mariana");
-  console.log("Separado:", r);
-}
-
-{
-  //exercicio5
-  function criar(a, b) {
-    let v = []; //cria um array vazio
-    for (let i = 0; a <= b; i++, a++) {
-      (v[i] = a), b;
-    }
-    return v;
-  }
-  let r = criar(5, 10);
-  console.log("Array:", r);
-}
-
-{
-  //exercicio6
-  function criar(a, b) {
-    let v = []; //cria um array vazio
-    for (let i = 0; a <= b; i++, a++) {
-      v[i] = a + "";
-    }
-    return v;
-  }
-  let r = criar(5, 10);
-  console.log("Array:", r);
-}
-
-{
-  //exercicio7
-  function somar(a, b) {
-    let v = []; //cria um array vazio
-    for (let i = 0; i < a; i++) {
-      v[i] = a[i] + b[i];
-    }
-    return;
-  }
-  let a = [5, 8, 3, 4, 9, 7];
-  let b = [3, 5, 9, 2, 3, 4];
-  let r = somar(a, b);
-  console.log("Soma:", r);
-}
+let a = [5, 8, 3, 4, 9, 7];
+let b = [3, 5, 9, 2, 3, 4];
+let r = somar(a, b);
+console.log("Soma:", r);
--- Algoritmos e Logica de Programação/Typescript/Aula 1/src/Exercicio1.ts
+++
@@ -1,8 +0,0 @@
-function converter (entrada:string){
-    const textos = entrada.split(",");
-    const nros:number[] = [];
-    for( let i = 0; i < textos.length; i++){
-        nros[i] = parseInt (textos[i]);
-    }
-    return nros;
-}
